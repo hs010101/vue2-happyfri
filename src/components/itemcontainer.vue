53adec4511fe40a87d0b822910a9cd72d84a83e2
hesen <hesupersen@gmail.com>
2024-12-13 17:07:39 +0800

    修改字体颜色

--- src/components/itemcontainer.vue
+++ src/components/itemcontainer.vue
@@ -163,7 +163,7 @@ export default {
     }
 	.item_title{
 		font-size: 0.65rem;
-		color: #00e;
+		color: #FFFFFF;
 		line-height: 0.7rem;
 	}
 	.item_list{
@@ -173,7 +173,7 @@ export default {
 		span{
 			display: inline-block;
 			font-size: 0.6rem;
-			color: #00e;
+			color: #FFFFFF;
 			vertical-align: middle;
 		}
 		.option_style{
